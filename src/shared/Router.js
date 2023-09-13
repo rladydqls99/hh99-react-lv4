5d83993d5e9a4bbe20330c50aadc354fb941630a
rladydqls99 <rladydqls99@naver.com>
2023-09-13 15:22:47 +0900

    1

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -4,15 +4,25 @@ import Login from "../pages/Login";
 import Join from "../pages/Join";
 import Main from "../pages/Main";
 import { CookiesProvider } from "react-cookie";
+import { getCookie } from "../cookies/cookies";
+import PrivateRoute from "./PrivateRoute";
 
 function Router() {
+  const access = getCookie("accessToken");
+  console.log(access);
+
   return (
     <CookiesProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />}></Route>
           <Route path="/join" element={<Join />}></Route>
-          <Route path="/main/:id" element={<Main />}></Route>
+          <Route
+            path="/main/:id"
+            element={
+              <PrivateRoute authenticated={access} component={<Main />} />
+            }
+          ></Route>
         </Routes>
       </BrowserRouter>
     </CookiesProvider>
